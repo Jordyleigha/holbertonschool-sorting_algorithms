034f44dba0ffadb5c08e1163cd0ea673eb7f3a1a
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-21 18:10:19 -0800

    fixing typos

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,37 +2,40 @@
 
 void quick_sort(int *array, size_t size)
 {
-    size_t pivot_index;  /* Declare pivot_index at the beginning*/
+	size_t pivot_index;
 
-    if (size <= 1) {
-        return;  /* Base case: array with 0 or 1 element is already sorted */
-    }
+	if (size <= 1)
+	{
+		return;
+	}
 
-    pivot_index = partition(array, size);
-    quick_sort(array, pivot_index);
-    quick_sort(array + pivot_index + 1, size - pivot_index - 1);
+	pivot_index = partition(array, size);
+	quick_sort(array, pivot_index);
+	quick_sort(array + pivot_index + 1, size - pivot_index - 1);
 }
 
-size_t partition(int *array, size_t size){
-    int pivot = array[size - 1];
-    int temp;
-    size_t i = 0, j;
+size_t partition(int *array, size_t size)
+{
+	int pivot = array[size - 1];
+	int temp;
+	size_t i = 0, j;
 
-    for (j = 0; j < size - 1; j++) {
-        if (array[j] <= pivot) {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-        }
-    }
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+		}
+	}
 
-    /* Swap pivot with element at i*/
-    temp = array[i];
-    array[i] = array[size - 1];
-    array[size - 1] = temp;
+	temp = array[i];
+	array[i] = array[size - 1];
+	array[size - 1] = temp;
 
-    print_array(array, size);
+	print_array(array, size);
 
-    return i;
+	return i;
 }
