41192b6ff5bc6713a4d4d6ea5d7391bbacda173c
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2023-12-21 20:47:04 -0600

    Update sort.h

--- sort.h
+++ sort.h
@@ -12,18 +12,19 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-size_t partition(int *array, size_t size);
-void quick_sort_rec(int *array, int start, int end);
-void swap(int *a, int *b);
+int partition(int *array, int low, int high, size_t size);
+void sort(int *array, int low, int high, size_t size);
+
 #endif
