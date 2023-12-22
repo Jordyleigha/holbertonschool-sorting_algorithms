ef9e899ce2f8fd67b99060b34b3820d1844fab9e
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-21 18:03:41 -0800

    rewriting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,10 +2,10 @@
 
 void quick_sort(int *array, size_t size)
 {
-    size_t pivot_index;
+    size_t pivot_index;  /* Declare pivot_index at the beginning*/
 
     if (size <= 1) {
-        return;
+        return;  /* Base case: array with 0 or 1 element is already sorted */
     }
 
     pivot_index = partition(array, size);
@@ -13,22 +13,22 @@ void quick_sort(int *array, size_t size)
     quick_sort(array + pivot_index + 1, size - pivot_index - 1);
 }
 
-size_t partition(int *array, size_t size)
-{
+size_t partition(int *array, size_t size){
     int pivot = array[size - 1];
     int temp;
     size_t i = 0, j;
 
     for (j = 0; j < size - 1; j++) {
         if (array[j] <= pivot) {
-            int temp = array[i];
+            temp = array[i];
             array[i] = array[j];
             array[j] = temp;
             i++;
         }
     }
 
-    int temp = array[i];
+    /* Swap pivot with element at i*/
+    temp = array[i];
     array[i] = array[size - 1];
     array[size - 1] = temp;
 
