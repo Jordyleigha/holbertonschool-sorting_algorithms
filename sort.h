3ed3739dac7bfb8fd96cfbef62da99b7efbdfcd5
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-21 18:16:13 -0800

    adding functions

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 size_t partition(int *array, size_t size);
-
+void quick_sort_rec(int *array, int start, int end);
+void swap(int *a, int *b);
 #endif
