222b023cfef082ccb1751c93767d6aea98f4d9b8
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-21 18:15:10 -0800

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,41 +1,76 @@
 #include "sort.h"
-
+/**
+ * quick_sort - Sorts a list of integers in ascending order using the Quick sort
+ * algorithm and the Lomuto partition scheme
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ * Return: Nothing
+ * Description: This function sorts an array of integers in ascending order
+ * using the Quick sort algorithm 
+ */
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot_index;
-
-	if (size <= 1)
-	{
+	if (array == NULL || size < 2)
 		return;
-	}
 
-	pivot_index = partition(array, size);
-	quick_sort(array, pivot_index);
-	quick_sort(array + pivot_index + 1, size - pivot_index - 1);
+	quick_sort_rec(array, 0, size - 1);
 }
-
-size_t partition(int *array, size_t size)
+/**
+ * quick_sort_rec - Sorts a list of integers in ascending
+ * 
+ * @array: pointer to array
+ * @start: start
+ * @end: end
+ */
+void quick_sort_rec(int *array, int start, int end)
 {
-	int pivot = array[size - 1];
-	int temp;
-	size_t i = 0, j;
+	int pivot, i, j;
 
-	for (j = 0; j < size - 1; j++)
+	if (start >= end)
+		return;
+
+	pivot = array[end];
+	i = start;
+	j = end - 1;
+
+	while (i <= j)
 	{
-		if (array[j] <= pivot)
-		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+		while (array[i] < pivot)
 			i++;
+		while (array[j] > pivot && j >= 0)
+			j--;
+		if (i <= j)
+		{
+			swap(&array[i], &array[j]);
+			print_array(array, end + 1);
+			i++;
+			j--;
+
 		}
 	}
+	swap(&array[i], &array[end]);
+	print_array(array, end + 1);
+	swap(&array[i], &array[end]);
+	print_array(array, end + 1);
+	quick_sort_rec(array, start, i - 1);
+	swap(&array[i], &array[end]);
+	print_array(array, end + 1);
+	quick_sort_rec(array, i + 1, end);
+	swap(&array[i], &array[end]);
+	print_array(array, end + 1);
+}
 
-	temp = array[i];
-	array[i] = array[size - 1];
-	array[size - 1] = temp;
+/**
+ * @swap - swap vars
+ * 
+ * @a: index pointer
+ * @b: index pointer 
+ */
+void swap(int *a, int *b)
+{
+	int temp;
 
-	print_array(array, size);
-
-	return i;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
